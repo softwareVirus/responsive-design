df8bf61037a224a0b0556c01c73e654e8232ee71
ibrahim halil sakli <63147096+softwareVirus@users.noreply.github.com>
2022-06-30 15:19:27 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -43,13 +43,11 @@ const theme = createTheme({
     }
   },
   palette: {
-    price: 'hsl(158, 36%, 37%)',
-    primary: {
-      text: 'hsl(228, 12%, 48%)'
-    }
+    price: 'hsl(158, 36%, 37%)'
   }
 })
 
+theme.palette.primary.text = 'hsl(228, 12%, 48%)'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
